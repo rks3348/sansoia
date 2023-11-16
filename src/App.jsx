0ac42477d6b9fed9f2ccc49c6828e04f9ee6daec
Raman Kumar <rks3348@yahoo.com>
2023-11-16 17:26:42 +0530

    coming soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const Section = () => {
+  return (
+    <section id="hero" className="d-flex">
+    <div className="hero-container">
+      <h1>Coming Soon</h1>
+      <h2>We're working hard to improve our website and we'll ready to launch after</h2>
+    </div>
+  </section>
+  );
+}
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Section />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
